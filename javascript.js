53121d57b7afa7bc89e0a28d5796ee81ef389d6d
David <davkh10@gmail.com>
2024-09-12 19:28:40 -0300

    Create and tested functions of calculator.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
+function add(a, b) {
+    return a + b;
+};
+
+function sub(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    let result = a / b;
+    if (result == NaN || result != Infinity) {
+        return result;
+    } else {
+        return "ERROR";
+    }
+};
 
